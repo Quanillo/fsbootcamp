231eb03f490e0d768926ff6b925be14ffa7beaa5
Juan <j.garrigos91@gmail.com>
2022-06-13 17:01:56 +0200

    Probando github

--- part1/src/index.js
+++ part1/src/index.js
@@ -26,6 +26,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   return <p>{props.msg} {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 }
 
