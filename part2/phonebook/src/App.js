6bea76f6aee951f3d8621d7c9e7f364e37e8a089
Juan <j.garrigos91@gmail.com>
2022-06-24 02:00:16 +0200

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 import { Filter } from './Components/Filter';
 import { PersonForm } from './Components/PersonForm';
 import { Persons } from './Components/Persons';
 
+
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-    { name: 'Peter Pheterson', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+  const [ usedFilter, setusedFilter] = useState(false)
   const [ filterList, setFilterList ] = useState(persons)
   const message = newName + ' is already added to phonebook'
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
@@ -33,6 +39,11 @@ const App = () => {
     })
 
     setFilterList(isInFilter)
+
+    if (isInFilter.lengt === 0)
+      setusedFilter(false)
+    else 
+      setusedFilter(true)
   }
 
   const handleSubmit = (event) => {
@@ -49,6 +60,8 @@ const App = () => {
     setNewNumber('')
   };
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -59,8 +72,13 @@ const App = () => {
       newName={newName} handleChangeNumber={handleChangeNumber} newNumber={newNumber} />
 
       <h2>Numbers</h2>
-      <Persons filterList={filterList} />
-    
+      {
+        usedFilter === false ?(
+          <Persons filterList={persons} />
+        ):( 
+          <Persons filterList={filterList} />
+        )
+      }
     </div>
   )
 }
